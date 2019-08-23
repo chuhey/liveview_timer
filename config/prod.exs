21dc52a016a5353c8f8d60eadfbb97be5bd0971a
Chuhei Ishigaki <chuhei1107@gmail.com>
2019-08-23 23:19:50 +0900

    change host name and port on production config.

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :live_view_counter, LiveViewCounterWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [host: "hotpink-repulsive-atlanticblackgoby.gigalixirapp.com", port: System.get_env("PORT") || 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production

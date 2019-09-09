0a58e6386ba53f8cb3507b62fd26ae1848e1e2a3
Chuhei Ishigaki <chuhei1107@gmail.com>
2019-09-09 15:02:36 +0900

    change phoenix liveview deps version

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule LiveViewCounter.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.3"},
+      {:phoenix, "~> 1.4.9"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.0"},
@@ -43,7 +43,7 @@ defmodule LiveViewCounter.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"}
+      {:phoenix_live_view, "~> 0.1.0"}
     ]
   end
 
